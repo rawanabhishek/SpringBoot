c557307d6328c05d1cb57e4619b3252447365ab3
rawanabishek <rawatabhishek2012@gmail.com>
2019-10-16 19:35:38 +0530

    Spring Boot

---
+++ course-api/src/main/java/com/bridgelabz/springboot/ApiApp.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApp.class, args);
+
+	}
+
+}
+
